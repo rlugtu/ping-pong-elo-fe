72be7b79f5f42f1574217dcd8a239c96d94b0a07
Ryan <ryan@Ryans-MBP.lan>
2024-01-22 02:45:45 -0600

    min-h-screen

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-    <h1 class="p-2 text-slate-300 text-6xl font-semibold h-[70px]">Ping Pong</h1>
+    <main class="min-h-screen relative">
+        <h1 class="p-2 text-slate-300 text-6xl font-semibold h-[70px]">Ping Pong</h1>
 
-    <LoadingScreen
-        v-if="!user"
-        class="absolute top-0 h-screen w-screen flex justify-center items-center"
-    ></LoadingScreen>
-    <RouterView v-else class="font-sans bg-gray-900 pt-6 min-h-[calc(100vh_-_100px)]" />
-    <Navbar v-if="user" class="fixed bottom-0 w-full bg-slate-700 h-[50px]"></Navbar>
+        <LoadingScreen
+            v-if="!user"
+            class="absolute top-0 h-screen w-screen flex justify-center items-center"
+        ></LoadingScreen>
+        <RouterView v-else class="font-sans bg-gray-900 pt-6 min-h-[calc(100vh_-_100px)]" />
+        <Navbar v-if="user" class="fixed bottom-0 w-full bg-slate-700 h-[50px]"></Navbar>
+    </main>
 </template>
 
 <script lang="ts" setup>
