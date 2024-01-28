aa4c11ed2c990141af57724c09bf622175a19d11
Ryan <ryan@Ryans-MBP.lan>
2024-01-28 16:21:16 -0600

    testing socket reconnect

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <main class="min-h-screen relative">
-        <h1 class="p-2 text-slate-300 text-4xl font-semibold">Ping Pong</h1>
+        <h1 class="p-2 text-slate-300 text-4xl font-semibold">Ping Pong <span>v1</span></h1>
 
         <RouterView class="font-sans bg-gray-900 pt-6 pb-[120px]" />
         <Navbar v-if="user" class="fixed bottom-0 w-full bg-slate-700 h-[80px]"></Navbar>
@@ -11,9 +11,9 @@
 import { useUserStore } from './stores/user'
 import { useAuthStore } from './stores/auth'
 import Navbar from './components/Navbar.vue'
-import { computed, watch } from 'vue'
+import { computed, onMounted, ref, watch } from 'vue'
 import router from './router'
-import { socket, socketSetup } from './socket'
+import { socket, socketSetup, state } from './socket'
 
 const userStore = useUserStore()
 const authStore = useAuthStore()
@@ -23,6 +23,7 @@ const userId = computed(() => {
 })
 
 const user = computed(() => userStore.user)
+const isConnected = computed(() => state.connected)
 
 watch(userId, async (userId) => {
     try {
@@ -36,5 +37,18 @@ watch(userId, async (userId) => {
     } catch (error) {
         router.push('/user-setup')
     }
+})
+
+onMounted(() => {
+    // for vue socket
+    document.addEventListener('visibilitychange', () => {
+        if (document.visibilityState === 'hidden') {
+            return
+        }
+
+        if (!state.connected) {
+            socket.connect()
+        }
+    })
 })
 </script>
