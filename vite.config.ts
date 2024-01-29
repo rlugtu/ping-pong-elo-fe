4e65908588421cd5201d8d9355e66a042bbfc41f
Ryan <ryan@Ryans-MBP.lan>
2024-01-28 21:03:57 -0600

    pwa info

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,42 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), VitePWA({ registerType: 'autoUpdate' })],
+    plugins: [
+        vue(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'Ping Pong',
+                short_name: 'PingPong',
+                description: 'Ping Pong Elo System',
+                theme_color: '#111827',
+                icons: [
+                    {
+                        src: '/public/img/icons/192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/public/img/icons/512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/public/img/icons/512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any'
+                    },
+                    {
+                        src: '/public/img/icons/512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'maskable'
+                    }
+                ]
+            }
+        })
+    ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
