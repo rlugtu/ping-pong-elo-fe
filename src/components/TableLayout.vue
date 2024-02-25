6a3871b4c2fd019f76c9b16ebdd5ffdc4ab59fc8
anthony.mercado <anthony.mercado@atmosphere.tv>
2024-02-24 18:29:34 -0600

    Create TableLayout

---
+++ src/components/TableLayout.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="grid grid-rows-2 gap-2.5 h-full">
+        <div class="px-6 h-full row-span-1 bg-blue-500">
+            <slot name="top" />
+        </div>
+        <div class="px-6 h-full row-span-1 bg-blue-500">
+            <slot name="bottom" />
+        </div>
+    </div>
+</template>
